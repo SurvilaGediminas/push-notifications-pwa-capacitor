d53f6176c66dcc132c76e42ce70236e4bd0ed572
Gediminas Survila <survila.gediminas@gmail.com>
2024-08-20 12:22:31 +0300

    port env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
